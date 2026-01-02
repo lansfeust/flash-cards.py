7e2fa410ac49b647f05bc61772dda27c58eeee57
lansfeust <lansfeust@hotmail.fr>
2026-01-02 08:41:37 +0100

    ajout du gif

---
+++ main.py
@@ -0,0 +1,61 @@
+""" 
+Titre : flash-Cards
+Nom du projet : flash-Cards
+
+Nom du fichier : NAME
+
+Date de la dernière révision : 02/01/2025
+
+Auteur(s) : EGO
+Révision N°: Version 0.1
+Client : maison
+
+#-------------------------------------------------
+name='Package_name',
+    version='Version _______ ',
+    packages=PackageList,PackageDirs
+    url='Aucun',
+    license='Libre',
+    author='EGO',
+    author_email='lansfeust@hotmail.fr',
+    description=' _______ '
+
+#-------------------------------------------------
+
+Fichiers du projet :
+    - main
+    - fenetre
+    - _______
+    - _______
+    - _______
+    - _______
+    - _______
+
+ """ 
+################################################
+#                Programme principal
+################################################
+
+# -----------------------------------------------
+#            Zone des 'imports' de modules
+# -----------------------------------------------
+import tkinter
+import time
+
+# ----------------------------------------------------
+#        Zone de déclaration des variables globales
+# ----------------------------------------------------
+_______ = _______
+
+# -------------------------------------------------------
+#        Zone de déclaration des modules ou des fonctions
+# -------------------------------------------------------
+
+def _______():
+    return _______
+
+# -------------------------------------------------------
+#                        PROGRAMME
+# -------------------------------------------------------
+
+time.sleep( 2 )
