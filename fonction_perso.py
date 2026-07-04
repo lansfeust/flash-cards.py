2cdd46a01cca49f5e2e31711ff062fbe9a588898
lansfeust <lansfeust@hotmail.fr>
2026-07-05 00:12:01 +0200

    Resolution de la fonction perso "confirmation"

---
+++ fonction_perso.py
@@ -0,0 +1,36 @@
+def confirmation(depart , si_oui , si_non):
+    print('-'*49)#Choix estetique
+    
+    while True:
+        print('\nVoici votre choix voulez-vous la concervé ?\n\n', depart)#Affiche le choix de l'utilisateur
+        choix = input('\nConservé ? oui , non \n\t')
+        
+        liste_autorisé = ('o',
+                            'O',
+                            'y',
+                            'yes',
+                            'oui')#Liste des actions qui veux dire 'OUI'
+        
+        if choix in liste_autorisé :#Si oui on quitte la fonction et passe a la suite du script
+            break
+
+        elif choix == 'n' or choix == 'non' or choix == 'no':# Si non , on re-pose la question
+
+            if type(si_non) is list :#petit scrypt qui debogue si c'est une liste
+
+                for liste in si_non :
+                    print( liste )
+                depart = input( ' ' )
+
+            else:
+                non = """\n {0}\n""".format(str(si_non))
+                depart = input( non )
+            
+        elif choix =='q' or choix == 'quit' :
+            exit()
+
+        else:# Si l'utilisateur ne rentre ni oui ni non , on recomance depuit le debut
+            continue
+
+if __name__ == '__main__':
+    print('-'*(7*7))
--- terminal_page_2.py
+++ terminal_page_2.py
@@ -6,6 +6,7 @@
 #		    Zone des 'imports' de modules
 # -----------------------------------------------
 from database import *
+from fonction_perso import *
 
 # ----------------------------------------------------
 #		Zone de déclaration des variables globales
@@ -24,13 +25,16 @@ class terminal_creation_cards:
     def affichage_famille(self):
         self.connection = connect_db()
         self.famille = get_famille( self.connection )#Extraction des familles
+        self.liste_famille = []
 
         temps_nombre_famille = get_nombre_famille( self.connection )#Version temporaire (une liste dans une liste)
         self.nombre_famille = int(temps_nombre_famille[0][0])#nombre extraite correctement
         
         print('\nListe de toutes les familles\n')
         for famille , id_famille in self.famille :
-            print(famille,'\t=>\t',id_famille)# affiche toutes les familles et le numéros qui correspond 
+            temporaire = str(famille)+'\t=>\t'+str(id_famille)
+            print(temporaire)# affiche toutes les familles et le numéros qui correspond 
+            self.liste_famille.append(temporaire)
         print('\n')
 
     def selection_famille(self):
@@ -51,6 +55,10 @@ class terminal_creation_cards:
         self.famille_choisi = id_famille
 
     def confirmation_famille(self):
+        confirmation(self.famille_choisi , None , self.liste_famille )
+
+    def old_confirmation_famille(self):
+        
         while True :
             print('êtes-vous sur de choisir ',self.famille_choisi,'?\n')
             choix = input("'o' pour confirmé 'n' pour recomancé 'q' pour quitté\n")
@@ -74,6 +82,23 @@ class terminal_creation_cards:
             else :
                 continue 
 
+    def question(self):
+        label ='\tPosé votre question ( 255 caractéres maximum est 3 caractéres minimum )\n'
+        question = input(label)
+        confirmation(question , None , label )
+    
+    def reponse(self):
+        question = input('\tPosé votre question ( 255 caractéres maximum est 3 caractéres minimum )')
+        pass
+
+    def securité_paragraphe(self , paragraphe):
+        
+        while True :
+            if len( paragraphe ) > 255 or len( paragraphe ) < 4 :
+                pass
+
+
+
 # -------------------------------------------------------
 #						PROGRAMME
 # -------------------------------------------------------
@@ -85,7 +110,8 @@ if __name__ == "__main__" :
     teste.selection_famille()
     teste.confirmation_famille()
 
-    ###estion de la question#####
+    ###Gestion de la question#####
+    teste.question()
 
 
 
